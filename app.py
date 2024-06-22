e9ff8480a9f87b449df26625b857b7e05ccd59b7
Trokitoshki <alex2905villa@gmail.com>
2024-06-21 19:49:03 -0500

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import joblib
-from sklearn.preprocessing import MinMaxScaler
 
 # Cargar el modelo entrenado
 model = joblib.load('xgboost_model.pkl')
@@ -16,19 +15,24 @@ st.write("""
 # Definir las entradas del usuario basadas en las características mencionadas
 NúmeroDeEstudiantesEsperados = st.number_input('Número de Estudiantes Esperados', min_value=0, max_value=1000, value=500)
 NúmeroDeEstudiantesPresentes = st.number_input('Número de Estudiantes Presentes', min_value=0, max_value=1000, value=500)
+IngresoMedio = st.number_input('Ingreso Medio', min_value=0, max_value=100000, value=25000)
+MatemáticaPromedio = st.number_input('Matemática Promedio (%)', min_value=0.0, max_value=100.0, value=50.0)
+LecturaPromedio = st.number_input('Lectura Promedio (%)', min_value=0.0, max_value=100.0, value=50.0)
+Temperatura = st.number_input('Temperatura', min_value=0.0, max_value=50.0, value=25.0)
+NúmeroDeDocentes = st.number_input('Número de Docentes', min_value=0, max_value=100, value=10)
+NivelDeSatisfacciónDeLosEstudiantes = st.number_input('Nivel de Satisfacción de los Estudiantes (%)', min_value=0.0, max_value=100.0, value=75.0)
 
 # Crear DataFrame con las entradas del usuario
 input_data = pd.DataFrame({
     'NúmeroDeEstudiantesEsperados': [NúmeroDeEstudiantesEsperados],
-    'NúmeroDeEstudiantesPresentes': [NúmeroDeEstudiantesPresentes]
+    'NúmeroDeEstudiantesPresentes': [NúmeroDeEstudiantesPresentes],
+    'IngresoMedio': [IngresoMedio],
+    'MatemáticaPromedio%': [MatemáticaPromedio],
+    'LecturaPromedio%': [LecturaPromedio],
+    'Temperatura': [Temperatura],
+    'NúmeroDeDocentes': [NúmeroDeDocentes],
+    'NivelDeSatisfacciónDeLosEstudiantes%': [NivelDeSatisfacciónDeLosEstudiantes]
 })
-
-# Paso 1: Escalado de las columnas numéricas (asumiendo que se utilizó MinMaxScaler)
-scaler = MinMaxScaler()
-numeric_columns = ['NúmeroDeEstudiantesEsperados', 'NúmeroDeEstudiantesPresentes']
-
-# Aplicar el escalado a las columnas numéricas
-input_data[numeric_columns] = scaler.fit_transform(input_data[numeric_columns])
 
 # Mostrar las entradas del usuario
 st.write('## Datos Introducidos:')
@@ -42,3 +46,4 @@ if st.button('Predecir'):
     except Exception as e:
         st.write("Error al realizar la predicción:")
         st.write(e)
+
